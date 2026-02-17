ad4cf257f26418d32545c322923f77aa5ecee22a
Faisal Bin Khurram <5salbin2005@gmail.com>
2026-02-17 10:47:44 +0530

    ready for deployment

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,38 +1,37 @@
-    import express from "express";
-    import { createServer } from "node:http";
-    import { Server } from "socket.io";
+import dotenv from 'dotenv';
+dotenv.config();
 
-    import mongoose from "mongoose";
-    import { connectToSocket } from "./controllers/socketManager.js";
+import express from "express";
+import { createServer } from "node:http";
+import mongoose from "mongoose";
+import cors from "cors";
 
-    import cors from "cors";
-    import userRoutes from "./routes/users.routes.js";
+import { connectToSocket } from "./controllers/socketManager.js";
+import userRoutes from "./routes/users.routes.js";
 
-    const app = express();
-    const server = createServer(app);
-    const io = connectToSocket(server);
+const app = express();
+const server = createServer(app);
+const io = connectToSocket(server);
 
+app.set("port", process.env.PORT || 8000);
 
-    app.set("port", (process.env.PORT || 8000))
-    app.use(cors());
-    app.use(express.json({ limit: "40kb" }));
-    app.use(express.urlencoded({ limit: "40kb", extended: true }));
+app.use(cors({
+  origin: "*",
+  methods: ["GET","POST","PUT","DELETE"]
+}));
 
-    app.use("/api/v1/users", userRoutes);
+app.use(express.json({ limit: "40kb" }));
+app.use(express.urlencoded({ limit: "40kb", extended: true }));
 
-    const start = async () => {
-        app.set("mongo_user")
-        const connectionDb = await mongoose.connect("mongodb+srv://zoomClone:faisalbin@zoomclone.78pxdgb.mongodb.net/?appName=zoomClone")
+app.use("/api/v1/users", userRoutes);
 
-        console.log(`MONGO Connected DB HOst: ${connectionDb.connection.host}`)
-        server.listen(app.get("port"), () => {
-            console.log("LISTENIN ON PORT 8000")
-        });
+const start = async () => {
+  const connectionDb = await mongoose.connect(process.env.MONGO_URI);
+  console.log(`MONGO Connected DB Host: ${connectionDb.connection.host}`);
 
+  server.listen(app.get("port"), () => {
+    console.log(`SERVER RUNNING ON PORT ${app.get("port")}`);
+  });
+};
 
-
-    }
-
-
-
-    start();
+start();
